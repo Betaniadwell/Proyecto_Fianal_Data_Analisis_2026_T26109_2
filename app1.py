84f7028193ad7c414f17d9c53fd44d27467506e6
Betaniadwell <168609630+Betaniadwell@users.noreply.github.com>
2026-06-10 19:48:16 -0300

    Fix typos in plot titles and labels

--- app1.py
+++ app1.py
@@ -21,6 +21,8 @@ plt.ylabel('Ingresos', fontsize=12)
 plt.legend(title='Producto', fontsize=10, title_fontsize=11)
 plt.tight_layout()
 st.pyplot(plt.gcf())
+plt.clf()
+
 
 
 # Estilo
@@ -36,9 +38,9 @@ sns.lineplot(data=df_cantidad_mensual, x="month", y="cantidad", hue="producto", 
 plt.tick_params(axis='both', labelsize=10)
 
 # Ajustes de título y ejes
-plt.title('Cantidades venmdidas  por mes altos productos', fontsize=14)
+plt.title('Cantidades vendidas  por mes altos productos', fontsize=14)
 plt.xlabel('Mes', fontsize=12)
-plt.ylabel('Canidades', fontsize=12)
+plt.ylabel('Cantidades', fontsize=12)
 plt.legend(title='Producto', fontsize=10, title_fontsize=11)
 plt.show()
 
